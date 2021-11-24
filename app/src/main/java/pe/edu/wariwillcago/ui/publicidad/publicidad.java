ab98a2cc3f6d936b883a7fd36a39d652cabcc6de
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2021-11-24 02:12:09 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/wariwillcago/ui/publicidad/publicidad.java
@@ -0,0 +1,66 @@
+package pe.edu.wariwillcago.ui.publicidad;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.edu.wariwillcago.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link publicidad#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class publicidad extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public publicidad() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment publicidad.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static publicidad newInstance(String param1, String param2) {
+        publicidad fragment = new publicidad();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_publicidad, container, false);
+    }
+}
